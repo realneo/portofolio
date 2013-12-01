a04c8aa9d36b363af51375801c2dfffa661d70f5
Nadhir Bahayan <nbahayan@gmail.com>
2013-12-01 04:07:22 +0300

    Adds Who we are , Wh, what we do and hire us pages

--- index.php
+++ index.php
@@ -20,9 +20,12 @@
                     <div id='sub_logo_text' class='RawengulkLight'>Portofolio</div>
                 </div><!-- logo_holder -->
                 <div id='top_menu'>
+                    
                     <div class='top_menu light_blue_bg'>
-                        <img src='img/software_icon.png' alt='Yoteyote Softwares' />
-                        <p>Softwares</p>
+                        <a href='#2'>
+                            <img src='img/software_icon.png' alt='Yoteyote Softwares' />
+                            <p>Softwares<p>
+                        </a>
                     </div>
                     
                     <div class='top_menu pink_bg'>
@@ -37,7 +40,7 @@
                     
                     <div class='top_menu purple_bg'>
                         <img src='img/3d_icon.png' alt='Yoteyote 3D Animations' />
-                        <p>Logos</p>
+                        <p>3D</p>
                     </div>
                     
                     <div class='top_menu blue_bg'>
@@ -56,49 +59,188 @@
                     </div>
                     
                     <div class='top_menu lighter_blue_bg'>
-                        <img src='img/contacts_icon.png' alt='Yoteyote Contacts' />
-                        <p>Hire Us</p>
+                        <a href='#3'>
+                            <img src='img/contacts_icon.png' alt='Yoteyote Contacts' />
+                            <p>Hire Us</p>
+                        </a>
                     </div>
                 </div><!-- top_menu -->
             </div><!-- top_bar -->
 
             <div class="content">
-                <div>
-                    <h1 class='RawengulkLight'>Our mission is to Help brands shine online.</h1>
+                <div class='right_side'>
+                    <h1 class='RawengulkLight'>Our mission is to Help brands shine.</h1>
+                    <p>
+                        <a href='#1' class='btn RawengulkRegular'>WHO WE ARE</a>
+                        <a href='#2' class='btn RawengulkRegular'>WHAT WE DO</a>
+                        <a href='#3' class='btn RawengulkRegular'>HIRE US</a>
+                    </p>
+                </div>
+                <div class='left_side'>
+                    <img src='img/yoteyoteman_03.png' alt='Yoteyote Man' /> 
                 </div>
                 
             </div>
+            <div id='up_btn'>UP</div>
         </section>
-
+        
+        <!------------------ WHO WE ARE ----------------------->
         <section id="1" class="window">
             <div class="content">
-                <h2>
-                project root<br/>
-                | -- css<br/>
-                | -- | -- main.css (compiled)<br/>
-                | -- js<br/>
-                | -- | -- <span class="highlight">jquery.windows.js</span><br/>
-                | -- scss<br/>
-                | -- | -- <span class="highlight">main.scss</span><br/>
-                | -- <span class="highlight">index.html</span>
-                </h2>
-            </div>
-        </section>
-
-        <section id="2" class="window">
-            <div class="content">
-                <h2>How it works</h2>
-                <div class="explanation">This is a light bootstrap that sets up:
-                    <ul>
-                        <li>- the basic SASS for sequential full screen windows</li>
-                        <li>- a jQuery plugin for managing their positions as the user scrolls</li>
-                    </ul>
-                    The plugin simply opens up a handy API for position management, provides callbacks, and will allow for window snapping.  No CSS is applied to the callee.
+                <div class='content_960'>
+                    <h1 class='RawengulkRegular'>WHO WE ARE</h1>
+                    <h2 class='RawengulkLight white font32'>We are a team of digital experts that get their kicks from using tech to make people fall in love with your brand.</h2>
+                </div>
+                <div class='half_cream'></div>
+                <div class='content_fluid'>
+                    <div class='personal_stats'>
+                        
+                        <div class='ps_outer_box purple_bg'>
+                            <div class='ps_inner_box'>
+                                <div class='ps_inner_box_icon'>
+                                    <img src='img/stats/codes_icon.png' alt='Lines of Codes' />
+                                </div>
+                                <div class='ps_inner_box_stat'>
+                                    <h2 class='RawengulkRegular font25 white'>Lines of Codes</h2>
+                                    <h2 class='RawengulkLight'>Nov 2013</h2>
+                                    <h1 class='RawengulkRegular'>275,650</h1>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class='ps_outer_box pink_bg'>
+                            <div class='ps_inner_box'>
+                                <div class='ps_inner_box_icon'>
+                                    <img src='img/stats/coffee_icon.png' alt='Lines of Codes' />
+                                </div>
+                                <div class='ps_inner_box_stat'>
+                                    <h2 class='RawengulkRegular font25 white'>Cups of Coffee</h2>
+                                    <h2 class='RawengulkLight'>Nov 2013</h2>
+                                    <h1 class='RawengulkRegular'>213</h1>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class='ps_outer_box yellow_bg'>
+                            <div class='ps_inner_box'>
+                                <div class='ps_inner_box_icon'>
+                                    <img src='img/stats/champ_icon.png' alt='Lines of Codes' />
+                                </div>
+                                <div class='ps_inner_box_stat'>
+                                    <h2 class='RawengulkRegular font25 white'>Project EULER</h2>
+                                    <h2 class='RawengulkLight'>Fast Solver</h2>
+                                    <h1 class='RawengulkRegular'>275,650</h1>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class='ps_outer_box light_green_bg'>
+                            <div class='ps_inner_box'>
+                                <div class='ps_inner_box_icon'>
+                                    <img src='img/stats/movies_icon.png' alt='Lines of Codes' />
+                                </div>
+                                <div class='ps_inner_box_stat'>
+                                    <h2 class='RawengulkRegular font25 white'>Watched Over</h2>
+                                    <h1 class='RawengulkRegular white'>38</h1>
+                                    <h2 class='RawengulkLight white'>Series</h2>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class='ps_outer_box dark_purpe_bg'>
+                            <div class='ps_inner_box'>
+                                <div class='ps_inner_box_icon'>
+                                    <img src='img/stats/certified_icon.png' alt='Lines of Codes' />
+                                </div>
+                                <div class='ps_inner_box_stat'>
+                                    <h2 class='RawengulkRegular font25 white'>Certified over</h2>
+                                    <h2 class='RawengulkLight'>Nov 2013</h2>
+                                    <h2 class='RawengulkLight white'>Learning Courses</h2>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class='ps_outer_box lighter_blue_bg'>
+                            <div class='ps_inner_box'>
+                                <div class='ps_inner_box_icon'>
+                                    <img src='img/stats/movies_icon.png' alt='Lines of Codes' />
+                                </div>
+                                <div class='ps_inner_box_stat'>
+                                    <h2 class='RawengulkRegular font25 white'>Number of Visitors</h2>
+                                    <h2 class='RawengulkLight'>Nov 2013</h2>
+                                    <h1 class='RawengulkRegular'>32,484</h1>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </section>
-
+        
+        <!------------------ WHAT WE DO ----------------------->
+        <section id="2" class="window">
+            <div class="content">
+                <div class='content_960'>
+                    <h1 class='RawengulkRegular'>What We Do</h1>
+                    <h2 class='RawengulkLight font28'>We tackle business problems with intelligence. We use cutting-edge tech and cutting-edge thinking to bring brands to life online and direct consumer behaviour.</h2>
+                </div>
+                <div class='content_fluid'>
+                    <ul>
+                        <li><img src='img/icons/128x128/heart.png' alt='Yoteyote | Branding' /><p class='RawengulkRegular'>Branding</p></li>
+                        <li><img src='img/icons/128x128/light_bulb.png' alt='Yoteyote | Ideas' /><p class='RawengulkRegular'>Idea Creation</p></li>
+                        <li><img src='img/icons/128x128/puzzle.png' alt='Yoteyote | Software Development' /><p class='RawengulkRegular'>Sofware Development</p></li>
+                        <li><img src='img/icons/128x128/world.png' alt='Yoteyote | Website Development' /><p class='RawengulkRegular'>Website Development</p></li>
+                        <li><img src='img/icons/128x128/paint_brush.png' alt='Yoteyote | Graphic Designing' /><p class='RawengulkRegular'>Graphic Designing</p></li>
+                        <li><img src='img/icons/128x128/volume_up.png' alt='Yoteyote | Social Media' /><p class='RawengulkRegular'>Social Media</p></li>
+                        <li><img src='img/icons/128x128/shopping_cart.png' alt='Yoteyote | E-Commerce' /><p class='RawengulkRegular'>E-Commerce</p></li>
+                        <li><img src='img/icons/128x128/webcam.png' alt='Yoteyote | 3D Animation' /><p class='RawengulkRegular'>2D / 3D Animation</p></li>
+                        <li><img src='img/icons/128x128/photo_camera.png' alt='Yoteyote | Photography' /><p class='RawengulkRegular'>Photography</p></li>
+                        <li><img src='img/icons/128x128/film.png' alt='Yoteyote | Videos' /><p class='RawengulkRegular'>Videos</p></li>
+                        <li><img src='img/icons/128x128/database.png' alt='Yoteyote | Hosting' /><p class='RawengulkRegular'>Hosting Solutions</p></li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </section>
+        
+        <!----------------- HIRE US --------------------------->
         <section id="3" class="window">
+            <div class="content">
+                <h1 class='RawengulkRegular'>Hire Us</h1>
+                <iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v3/realneo.ge50cop9.html?secure=1#19/-6.81629018679672/39.28906202316284'></iframe>
+                <table width='100%'>
+                    <tr>
+                        <td width='30%'>
+                            <img src='img/icons/128x128/map_pin.png' alt='Yoteyote | Location'/>
+                            <span class='RawengulkRegular font18' style='display:block'>
+                                HEAD OFFICE<br />
+                                1st Floor Elite Tower<br />
+                                Azikiwe Street<br />
+                                Dar es Salaam , Tanzania
+                            </span>
+                        </td>
+                        <td width='30%'>
+                            <img src='img/icons/128x128/smart_phone.png' alt='Yoteyote | Call'/>
+                            <span class='RawengulkRegular font18' style='display:block'>
+                                GIVE US A CALL<br />
+                                +255 787 487333
+                            </span>
+                        </td>
+                        <td width='30%'>
+                            <img src='img/icons/128x128/email.png' alt='Yoteyote | Email'/>
+                            <span class='RawengulkRegular font18' style='display:block'>
+                                DROP US AN EMAIL<br />
+                                info@yoteyote.com
+                            </span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+
+        
+        <section id="4" class="window">
             <pre>
                 $('.window').windows({
                     snapping: true,
@@ -117,7 +259,7 @@
             </pre>
         </section>
 
-        <section id="4" class="window">
+        <section id="5" class="window">
             <div class="content">
                 <h1>api</h1>
                 <pre>
@@ -134,8 +276,8 @@
                 </pre>
             </div>
         </section>
-
-        <section id="5" class="window">
+<!--
+        <section id="6" class="window">
             <div class="content">
                 <a href="https://github.com/nick-jonas/nick-jonas.github.com/tree/master/windows" title="Download Me!" id="btn-wrap">
                     <span class="title">Download Me</span>
@@ -150,7 +292,7 @@
                 <iframe src="http://ghbtns.com/github-btn.html?user=nick-jonas&repo=windows&type=fork&count=true&size=large" allowtransparency="true" frameborder="0" scrolling="0" width="156px" height="30px" style="margin:50px 0 0 50px;"></iframe>
 
             </div>
-        </section>
+        </section> -->
 
         <script>
 
@@ -161,7 +303,7 @@
                 $windows.windows({
                     snapping: true,
                     snapSpeed: 500,
-                    snapInterval: 1100,
+                    snapInterval: 400,
                     onScroll: function(s){},
                     onSnapComplete: function($el){},
                     onWindowEnter: function($el){}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
 $(document).ready(function(){
-    $('.top_menu').mouseover(function(){
-        $(this).animate({ "top": "50px" }, "slow" );
+    
+    //top menu animation
+    $('.top_menu').hover(function(){
+        $(this).stop().animate({'padding-top' : '60px'}, 250);
+        $("p", this).stop().delay(50).fadeIn();
     },function(){
-        
+        $(this).stop().animate({'padding-top' : '0px'}, 250);
+        $("p", this).stop().delay(50).fadeOut();
     });
+    
+    // top menu scroll animation
+    var $root = $('html, body');
+    $('.top_menu a').click(function() {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 800);
+        return false;
+    });
+    
+    //button animation
+    $('a.btn').hover(function(){
+        $(this).stop().animate({'border-color':'#fff'});
+    });
+    
+    // btn scroll animation
+    var $root = $('html, body');
+    $('a.btn').click(function() {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 800);
+        return false;
+    });
+    
+    // PERSONAL STATS **************************************/
+    $('.ps_inner_box').hover(function(){
+        $(this).stop().animate({'top' : '-250px'});
+    },function(){
+        $(this).stop().animate({'top' : '0px'});
+    });
+    
 });
